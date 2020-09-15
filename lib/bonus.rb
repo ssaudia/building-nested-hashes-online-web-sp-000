3be97e774f2320498f962c60fd078aefb5a6a309
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-15 20:02:15 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -9,13 +9,13 @@ epic_tragedy = {
        {name: "Benvolio", age: "17", attitude: "worried"},
        {name: "Steven", age: "30", attitude: "confused"}
     ]
- }, 
+ },
  :capulet => {
     :patriarch => {name: "Lord Capulet", age: "50"},
     :matriarch => {name: "Lady Capulet", age: "51"},
     :heroine => {name: "Juliet", age: "15", status: "alive"},
     :heroine_friends => [
-        {name: "Mercutio", age: "18", attitude: "hot-headed"}, 
+        {name: "Mercutio", age: "18", attitude: "hot-headed"},
         {name: "Nurse", age: "44", attitude: "worried"}
     ]
  }
