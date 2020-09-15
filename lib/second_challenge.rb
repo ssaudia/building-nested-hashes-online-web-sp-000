688992c6f5d18344e3b8eace922bf8e433e5d5fa
Saudia Schwegler <saudiaschwegler@gmail.com>
2020-09-15 20:05:13 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,18 @@
-epic_tragedy = {
-   :montague => {
+
+def second_challenge
+  epic_tragedy = {
+    :montague => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-   },
-   :capulet => {
+      },
+    :capulet => {
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-   }
-}
+      }}
+
+  
+end
